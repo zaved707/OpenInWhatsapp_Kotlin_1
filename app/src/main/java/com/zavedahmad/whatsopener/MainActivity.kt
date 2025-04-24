585f8402a96622d89072244b405158f13be8aa3b
Zaved <zavedahmadca707@gmail.com>
2025-04-25 01:45:11 +0530

    removed countryCodes.json and updated mainactivity to not keep screen awake

--- app/src/main/java/com/zavedahmad/whatsopener/MainActivity.kt
+++ app/src/main/java/com/zavedahmad/whatsopener/MainActivity.kt
@@ -6,13 +6,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.lifecycle.viewmodel.compose.viewModel
 
+
 import com.zavedahmad.whatsopener.ui.theme.WhatsOpenerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+//        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         setContent {
             val viewModel: MainViewModel = viewModel()
             WhatsOpenerTheme {
